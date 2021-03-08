2ceed2fede041b8076bc62be80cae3bcaf21b642
itamar <itamardprf@gmail.com>
2021-03-08 19:37:48 -0300

    fix: specify now can receive any Subtype of MOI.AbstractOptimizer

--- src/formulations.jl
+++ src/formulations.jl
@@ -49,7 +49,7 @@ function Base.show(io::IO, m::SubproblemForm)
     return
 end
 
-function _specify!(sp::SubproblemForm, lm::Real, um::Real, opt::Union{MOI.OptimizerWithAttributes, MOI.AbstractOptimizer})
+function _specify!(sp::SubproblemForm, lm::Real, um::Real, opt::Union{MOI.OptimizerWithAttributes, Type{<:MOI.AbstractOptimizer}})
     setlowermultiplicity!(sp.annotation, lm)
     setuppermultiplicity!(sp.annotation, um)
     setoptimizerbuilder!(sp.annotation, MOI._instantiate_and_check(opt))
@@ -66,7 +66,7 @@ end
 
 function specify!(
     sp::SubproblemForm; lower_multiplicity::Real = 1, 
-    upper_multiplicity::Real = 1, solver::Union{Nothing, Function, MOI.OptimizerWithAttributes, MOI.AbstractOptimizer} = nothing
+    upper_multiplicity::Real = 1, solver::Union{Nothing, Function, MOI.OptimizerWithAttributes, Type{<:MOI.AbstractOptimizer}} = nothing
 )
     _specify!(sp, lower_multiplicity, upper_multiplicity, solver)
     return
