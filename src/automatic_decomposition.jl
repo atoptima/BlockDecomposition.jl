83a1b118ae3f674fb7c44bc8886381d776a64f62
David Meichel <davidmeichel@gmx.de>
2021-04-12 14:10:37 +0200

    Improve structure detection procedure
    Now for every subset S of the index sets, two structures
    are detected:
    1. All constraints index by at least set from S are linking
    2. All constraints not indexed by any set from S are linking
    
    Formatting of the code was improved, too.

--- src/automatic_decomposition.jl
+++ src/automatic_decomposition.jl
@@ -1,7 +1,9 @@
 # Decomposes the given JuMP Model automatically
 function decompose(model::JuMP.Model)
     model.ext[:decomposition_structure] = BlockDecomposition.get_best_block_structure(model)
-    decomposition_axis = BlockDecomposition.Axis(1:length(model.ext[:decomposition_structure].blocks))
+    decomposition_axis = BlockDecomposition.Axis(
+        1:length(model.ext[:decomposition_structure].blocks)
+    )
     decomposition = BlockDecomposition.decompose_leaf(
         model,
         BlockDecomposition.DantzigWolfe,
@@ -30,8 +32,11 @@ function get_best_block_structure(model::JuMP.Model)
         relative_border_area_scores = BlockDecomposition.relative_border_area_scores(block_structures)
         result = block_structures[argmin(relative_border_area_scores)]
     else
-        error("Score type ", score_type, " for automatic decomposition is not defined.
-        The available scores are: White Score: 0, Block Border Score: 1, Relative Border Area Score: 2")
+        error(
+            "Score type ", score_type, " for automatic decomposition is not defined.
+            The available scores are:
+            White Score: 0, Block Border Score: 1, Relative Border Area Score: 2"
+        )
     end
     return result
 end
@@ -42,8 +47,14 @@ function get_all_block_structures(model::JuMP.Model)
     block_structures = Array{BlockStructure,1}()
     axesSets = collect(powerset(collect(constraints_and_axes.axes)))
     for axes in axesSets
-        block_structure = get_block_structure(axes, constraints_and_axes, model)
-        push!(block_structures, block_structure)
+        block_structure0, block_structure1 = get_block_structure(axes, constraints_and_axes, model)
+        # Only add block structures that where not found so far
+        if !structure_exists(block_structures, block_structure0)
+            push!(block_structures, block_structure0)
+        end
+        if !structure_exists(block_structures, block_structure1)
+            push!(block_structures, block_structure1)
+        end
     end
     return block_structures
 end
@@ -63,11 +74,25 @@ struct BlockStructure
     constraints_and_axes::Constraints_and_Axes
     master_constraints::Set{JuMP.ConstraintRef}
     master_sets::Array{BlockDecomposition.Axis,1}
+    # Invert linked is true iff the linking constraints are the ones not indexed
+    # by any set from master_sets
+    invert_linking::Bool
     blocks::Array{Set{JuMP.ConstraintRef},1}
     graph::MetaGraph
 end
 
-
+# Returns true if and only if block_structure already exists in block_structures
+function structure_exists(
+    block_structures::Array{BlockStructure,1},
+    block_structure::BlockStructure
+)
+    for bs in block_structures
+        if bs.master_constraints == block_structure.master_constraints
+            return true
+        end
+    end
+    return false
+end
 
 # This score is described in: Bergner, Martin, et al. 
 # "Automatic Dantzig–Wolfe reformulation of mixed integer programs."
@@ -140,7 +165,8 @@ function white_scores(block_structures::Array{BlockStructure,1})
 end
 
 function _get_white_score(block_structure::BlockStructure)
-    n_master = length(block_structure.constraints_and_axes.variables) * length(block_structure.master_constraints)
+    n_master = length(block_structure.constraints_and_axes.variables) *
+        length(block_structure.master_constraints)
     n_blocks = 0
     variables_in_block = Set{MOI.VariableIndex}()
     for block in block_structure.blocks
@@ -153,7 +179,8 @@ function _get_white_score(block_structure::BlockStructure)
         end
         n_blocks = n_blocks + length(variables_in_block)*length(block)
     end
-    coefficient_matrix_size = length(block_structure.constraints_and_axes.constraints) * length(block_structure.constraints_and_axes.variables)
+    coefficient_matrix_size = length(block_structure.constraints_and_axes.constraints) *
+        length(block_structure.constraints_and_axes.variables)
     black_score = (n_master+n_blocks)/coefficient_matrix_size
     white_score = 1-black_score
     return white_score
@@ -198,7 +225,8 @@ function _get_constraint_reference(model::JuMP.Model, k)
         axs = axes(model.obj_dict[k])
         return JuMP.Containers.DenseAxisArray(model.obj_dict[k], axs...)
     else
-        return error("Type of constraint reference can not be handled.") # Can this case happen?
+        # Can this case happen?
+        return error("Type of constraint reference can not be handled.")
     end
 end
 
@@ -236,7 +264,9 @@ function _add_constraint!(
     o.constraints_to_variables[c] = _get_variables_in_constraint(model, c)
 end
 
-function _get_axes_of_constraint(reference_constraints_name::T) where T <: JuMP.Containers.DenseAxisArray
+function _get_axes_of_constraint(
+    reference_constraints_name::T
+) where T <: JuMP.Containers.DenseAxisArray
     axes_of_constraint = Array{BlockDecomposition.Axis,1}()
     for a in reference_constraints_name.axes
         if a != 1   # Axes of the form 1:1 do not matter (single constraints)
@@ -255,29 +285,45 @@ function _get_variables_in_constraint(model::JuMP.Model, constraint::JuMP.Constr
     return variables
 end
 
+# Computes for the given axes two decomposition structures:
+# In the first one (block_structure0) all constraints *indexed by at least one* element from
+# axes are in the master, in the second one (block_structure1), all constraints *not indexed
+# by any* element from axes are in the master
 function get_block_structure(
     axes::Array{<:Axis,1},
     constraints_and_axes::Constraints_and_Axes,
     model::JuMP.Model,
 )
-    vertices = Set{JuMP.ConstraintRef}()
-    master_constraints = Set{JuMP.ConstraintRef}()
-    connected_components = Set{Set{JuMP.ConstraintRef}}()
-
+    vertices = [Set{JuMP.ConstraintRef}(), Set{JuMP.ConstraintRef}()]
+    master_constraints = [Set{JuMP.ConstraintRef}(),Set{JuMP.ConstraintRef}()]
     for c in keys(constraints_and_axes.constraints_to_axes)
-        # Check if constraints are constructed over at least one set which is contained in axes
+        # Master constraints are constructed over at least one set which is contained in axes
         # if axes is empty annotate everything as master
         if  isempty(axes) || !isempty(intersect(axes, constraints_and_axes.constraints_to_axes[c]))
-            push!(master_constraints, c)
+            push!(master_constraints[1], c)
         else
-            push!(vertices, c)
+            push!(vertices[1], c)
+        end
+        # Master constraints are constructed over no set which is contained in axes
+        # To do: ensure that no BBD is created where the master is empty
+        if  size(axes)[1] == length(constraints_and_axes.axes) || isempty(intersect(axes, constraints_and_axes.constraints_to_axes[c]))
+            push!(master_constraints[2], c)
+        else
+            push!(vertices[2], c)
         end
     end
 
-    graph = _create_graph(vertices, constraints_and_axes)
+    graph = _create_graph(vertices[1], constraints_and_axes)
     blocks = _get_connected_components!(graph)
-    block_structure = BlockStructure(constraints_and_axes, master_constraints, axes, blocks, graph)
-    return block_structure
+    block_structure1 = BlockStructure(
+        constraints_and_axes, master_constraints[1], axes, false, blocks, graph
+    )
+    graph = _create_graph(vertices[2], constraints_and_axes)
+    blocks = _get_connected_components!(graph)
+    block_structure2 = BlockStructure(
+        constraints_and_axes, master_constraints[2], axes, true, blocks, graph
+    )
+    return block_structure1, block_structure2
 end
 
 function _get_connected_components!(graph::MetaGraph)
@@ -314,7 +360,9 @@ function _create_graph(
                 constraints_and_axes.constraints_to_variables[v1],
                 constraints_and_axes.constraints_to_variables[v2],
             )
-            isempty(intersection) ? true : add_edge!(graph, graph[v1, :constraint_ref], graph[v2, :constraint_ref])
+            if !isempty(intersection)
+                add_edge!(graph, graph[v1, :constraint_ref], graph[v2, :constraint_ref])
+            end
         end
     end
     return graph
