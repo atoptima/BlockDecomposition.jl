bceca4c15709333c79a4f446cbfa7825f3efcf47
Lara Pontes <laradicp@gmail.com>
2021-06-21 16:56:26 -0300

    fix attribute name

---
+++ src/spsinfo.jl
@@ -0,0 +1,18 @@
+abstract type AbstractColumnInfo end
+struct SpsInfo <: MOI.AbstractModelAttribute end
+struct SpInfo
+    columns_info::Vector{AbstractColumnInfo}
+end
+
+getsolutions(model, k) = MOI.get(model, SpsInfo())[k].columns_info
+
+function MOI.set(
+    dest::MOIU.UniversalFallback, attribute::SpsInfo, value::Vector{SpInfo}
+)
+    dest.modattr[attribute] = value
+    return
+end
+
+function MOI.get(dest::MOIU.UniversalFallback, attribute::SpsInfo)
+    return get(dest.modattr, attribute, nothing)
+end
