0dbe0e47dde165f7fe62abd4e5aee4b8dddcae99
Lara di Cavalcanti Pontes <34425678+laradicp@users.noreply.github.com>
2021-07-20 06:38:58 -0300

    Add methods `customvars!` and `customconstrs!` for single custom data (#64)
    * use Type{AbstractCustomData} and add methods without vector
    
    * revert usage of Type{AbstractCustomData}
    
    * change parameter name
    
    * add tests

--- src/customdata.jl
+++ src/customdata.jl
@@ -2,19 +2,27 @@ struct CustomVars <: MOI.AbstractModelAttribute end
 struct CustomConstrs <: MOI.AbstractModelAttribute end
 
 """
+    customvars!(model, customvar::DataType)
     customvars!(model, customvars::Vector{DataType})
 
 Set the possible custom data types of variables in a model.
 """
+customvars!(model, customvar::DataType) = MOI.set(
+    model, CustomVars(), [customvar]
+)
 customvars!(model, customvars::Vector{DataType}) = MOI.set(
     model, CustomVars(), customvars
 )
 
 """
+    customconstrs!(model, customconstr::DataType)
     customconstrs!(model, customconstrs::Vector{DataType})
 
 Set the possible custom data types of constraints in a model.
 """
+customconstrs!(model, customconstr::DataType) = MOI.set(
+    model, CustomConstrs(), [customconstr]
+)
 customconstrs!(model, customconstrs::Vector{DataType}) = MOI.set(
     model, CustomConstrs(), customconstrs
 )
@@ -48,9 +56,9 @@ function MOI.set(
 end
 
 function MOI.get(dest::MOIU.UniversalFallback, attribute::CustomVars)
-    return get(dest.modattr, attribute, nothing)
+    return get(dest.modattr, attribute, [])
 end
 
 function MOI.get(dest::MOIU.UniversalFallback, attribute::CustomConstrs)
-    return get(dest.modattr, attribute, nothing)
+    return get(dest.modattr, attribute, [])
 end
--- test/customdata.jl
+++ test/customdata.jl
@@ -19,11 +19,21 @@ function test_custom_data()
     customvars!(model, [MyCustomVarData1, MyCustomVarData2])
     customconstrs!(model, [MyCustomCutData1, MyCustomCutData2])
 
-    @testset "Custom data" begin
+    @testset "Add custom data vector" begin
         @test customvars(model)[1] == MyCustomVarData1
         @test customvars(model)[2] == MyCustomVarData2
         @test customconstrs(model)[1] == MyCustomCutData1
         @test customconstrs(model)[2] == MyCustomCutData2
+    end
+
+    customvars!(model, MyCustomVarData2)
+    customconstrs!(model, MyCustomCutData2)
+
+    @testset "Add single custom data" begin
+        @test length(customvars(model)) == 1
+        @test customvars(model)[1] == MyCustomVarData2
+        @test length(customconstrs(model)) == 1
+        @test customconstrs(model)[1] == MyCustomCutData2
     end
 
     return
