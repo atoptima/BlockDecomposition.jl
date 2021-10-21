2aaaaf7aaaa1ec12fe6752761f41dd4e6bf433bf
David Meichel <davidmeichel@gmx.de>
2021-10-21 09:12:50 +0200

    Use random numbers in the test for the white score

--- test/automatic_dantzig_wolfe.jl
+++ test/automatic_dantzig_wolfe.jl
@@ -1,20 +1,23 @@
 function test_automatic_dantzig_wolfe()
-    @testset "Example AP automatic Dantzig-Wolfe decomposition" begin
-        model, x, cov, knp = example_assignment_automatic_dantzig_wolfe()
+    d = GapToyData(30,4)
+    @testset "Classic GAP automatic Dantzig-Wolfe (white score)" begin
+        model, x, cov, knp = generalized_assignment_automatic_dantzig_wolfe(
+            d,
+            BlockDecomposition.white_score
+        )
         try
             JuMP.optimize!(model)
         catch e
             @test e isa NoOptimizer
         end
-        # all constraints build over the set machines are master constraints
-        # (this is due to the result of the automatic decomposition using the "white score")
         machines = 1:4
         jobs = 1:30
+        # all constraints build over the set of machines are master constraints
         for j in jobs
             cov_ann = BD.annotation(model, cov[j])
             test_annotation(cov_ann, BD.DwPricingSp, BD.DantzigWolfe, 1, 1)
             for m in machines
-                x_ann = BD.annotation(model, x[m,j])
+                x_ann = BD.annotation(model, x[m, j])
                 test_annotation(cov_ann, BD.DwPricingSp, BD.DantzigWolfe, 1, 1)
             end
         end
